65d842383c6cb7cbb0436def5c4f0d1e6be096d2
Raleigh Wayland <raleigh.wayland@stormapp.com>
2021-02-01 10:34:38 -0700

    Updating packages

--- src/complete/DropdownExercise/Dropdown.js
+++ src/complete/DropdownExercise/Dropdown.js
@@ -1,20 +1,27 @@
-import React, {useState} from 'react';
-import MenuButton from '../../components/MenuButton';
-import {Options, Option, DropdownContainer} from '../../components/styled-components';
+import React, { useState } from "react";
+import MenuButton from "../../components/MenuButton";
+import {
+  Options,
+  Option,
+  DropdownContainer,
+} from "../../components/styled-components";
 
-const Dropdown = ({options, children}) => {
+const Dropdown = ({ options, children }) => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <DropdownContainer>
       <MenuButton onClick={() => setIsOpen(!isOpen)} />
       {isOpen && (
         <Options>
-          {options && options.length > 0 ? options.map(item => <Option key={item}>{item}</Option>) : children}
+          {options?.length
+            ? options.map((item) => <Option key={item}>{item}</Option>)
+            : children}
         </Options>
       )}
     </DropdownContainer>
   );
 };
-Dropdown.Item = ({children}) => <Option>{children}</Option>;
+
+Dropdown.Item = ({ children }) => <Option>{children}</Option>;
 
 export default Dropdown;
--- src/exercises/DropdownExercise/Dropdown.js
+++ src/exercises/DropdownExercise/Dropdown.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import MenuButton from '../../components/MenuButton';
-import {Options, Option, DropdownContainer} from '../../components/styled-components';
+import React from "react";
+import MenuButton from "../../components/MenuButton";
+import {
+  Options,
+  Option,
+  DropdownContainer,
+} from "../../components/styled-components";
 
 const Dropdown = () => (
   <DropdownContainer>
